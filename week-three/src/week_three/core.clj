51c3e4fd51a5ae6e350580a4ce40cc991c93e782
Ctopham <craigmtopham@gmail.com>
2020-05-17 11:55:55 -0400

    updating

--- week-three/src/week_three/core.clj
+++ week-three/src/week_three/core.clj
@@ -8,20 +8,25 @@
   (comp (partial apply str) reverse str))
 (reverse-concat "hello" "there")
 
-(defn functwo [x n]
+(defn coll-div [x n]
   ;takes a collection and a int, this function will divide the collection
   ;the number of times if n. ex  [1 2 3 4] with n as 2 would be [1 2] [3 4]
   (let [temp (partition n n x)]
     (println "temp" temp)
     (for [i (range n)] (map #(nth %1 i) temp))))
 
-(functwo [1 2 3 4] 2)
+(coll-div [1 2 3 4] 2)
 
-(defn functhree [coll n]
+(defn inc-add [coll n]
   ; this function takes a sequence and a int.
   ;ever iteration n will increase by 1 and then that number will be added to
   ;the next  index
   ; example args[[1 2] 2] would be [3 5]
   (map + coll (iterate inc n)))
 
-(functhree [1 2 ] 2)
+(inc-add [1 2 ] 2)
+
+
+(= '(3 5) (inc-add [1 2 ] 2))
+(= '((1 3) (2 4)) (coll-div [1 2 3 4] 2 ))
+ (= "erehtolleh" (reverse-concat "hello" "there"))
--- week-three/test/week_three/core_test.clj
+++ week-three/test/week_three/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [week-three.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+;;Craig Topham -  Week 1 Tests
+(deftest reverse-concat-test
+  (is (= "erehtolleh" (reverse-concat "hello" "there"))))
+
+(deftest coll-div-test
+  (is (= '((1 3) (2 4)) (coll-div [1 2 3 4] 2 ))))
+
+(deftest inc-add-test
+  (is (= '(3 5) (inc-add [1 2 ] 2))))
