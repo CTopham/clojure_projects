169244d619507c15b0a0a91609f00be6278b797a
Craig <meenotop@gmail.com>
2020-05-22 13:22:08 -0400

    fixing the folders

---
+++ week-4/src/week_4/core.clj
@@ -0,0 +1,6 @@
+(ns week-4.core)
+
+(defn new-method
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ week-4/test/week_4/core_test.clj
@@ -0,0 +1,7 @@
+(ns week-4.core-test
+  (:require [clojure.test :refer :all]
+            [week-4.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
